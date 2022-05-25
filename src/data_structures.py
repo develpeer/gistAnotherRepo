9b74bd1a61852ba66a02374b58037ca2e3855875
Dev E L'Peer <102568326+develpeer@users.noreply.github.com>
2022-05-24 21:11:02 -0700

    more data structures

--- src/data_structures.py
+++ src/data_structures.py
@@ -1,6 +1,8 @@
 ##
 # List prepend test
 ##
+
+import math
 list_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(f"Before prepend list_1 : {list_1}")
 list_1[:0] = [666]
@@ -20,9 +22,35 @@ print("-=-" * 25)
 # Square all digits using a generator
 print([x ** 2 for x in range(10)])
 # Square all even digits
-print([(x,x ** 2) for x in range(10) if not x % 2])
+print([(x, x ** 2) for x in range(10) if not x % 2])
 # this expression is a generator
-gee = (x*2 for x in range(10) )
+gee = (x * 2 for x in range(10))
 print(next(gee))  # first element
 print(next(gee))  # second element
 print(*gee)  # remaining elements
+
+t = ("hello", None)
+print(t)
+u = ("hello",)
+v = ("hello",)
+
+print(t == v, u == v)
+a = set('tyler')
+b = set('nol')
+print("Set Diff:", a - b)
+print("Set Union:", a | b)
+print("Set intersection:", a & b)
+print("Set XOR", (a | b) - (a & b))
+print("Set XOR", a ^ b)
+
+#zip joins two lists sequentially
+q = ['name', 'quest', 'favorite color']
+a = ['lancelot', 'the holy grail', 'blue']
+for k, v in zip(q, a):
+    print(k, v)
+print(dict(zip(q, a)))
+
+#filtering a list using agenrator
+raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5, float('NaN'), 47.8]
+filtered_data = [(k+1) for k in raw_data if not math.isnan(k)]
+print("Filtered data is ", filtered_data)
