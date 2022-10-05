6ccff199de8b48a0cc03205c9873deeb3730e680
Dev E L'Peer <102568326+develpeer@users.noreply.github.com>
2022-10-04 17:44:34 -0700

    Splitting data Structures into 2 files

--- src/data_structure_dictionaries_and_sets.py
+++ src/data_structure_dictionaries_and_sets.py
@@ -1,41 +1,18 @@
+# the one and only Dev.E.L'Peer  https://github.com/develpeer
 ##
-# List prepend test
+# A bunch of examples explaining dictionaries and related 
+# concepts like  in python
 ##
 
 import math
 
-list_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(f"Before prepend list_1 : {list_1}")
-list_1[:0] = [666]
-print(f"After prepend list_1 : {list_1}")
-list_1 = [665] + list_1
-print(f"After prepend list_1 : {list_1}")
-list_1[:] = 664, *list_1
-print(f"After prepend list_1 : {list_1}")
-list_1.insert(0, 663)
-print(f"After prepend list_1 : {list_1}")
-list_1.insert(2, 664.5)
-print(f"After insert list_1 : {list_1}")
-list_1[4:4] = 655.5,
-print(f"After insert list_1 : {list_1}")
-
-print("-=-" * 25)
-# Square all digits using a generator
-print([x ** 2 for x in range(10)])
-# Square all even digits
-print([(x, x ** 2) for x in range(10) if not x % 2])
-# this expression is a generator
-gee = (x * 2 for x in range(10))
-print(next(gee))  # first element
-print(next(gee))  # second element
-print(*gee)  # remaining elements
-
+#list equality?
 t = ("hello", None)
-print(t)
 u = ("hello",)
 v = ("hello",)
+print(f"t = {t}, t==v :{t == v}, u==v :{u == v}")
 
-print(t == v, u == v)
+#sets
 a = set('tyler')
 b = set('nol')
 print("Set Diff:", a - b)
@@ -74,3 +51,19 @@ try:
     print (None < "b")
 except TypeError:
     print ("Comparing objects of different types with < or > is legal provided that the objects have appropriate comparison methods")
+
+
+########################################################
+# Understanding maps and lambda functions
+# lambda functions are just shortcut functions. 
+# Maps take two arguments a function, and a list, and 
+# then map the function to members of the list (or maybe even a tuple?) 
+# ....
+# list () = []
+########################################################
+def doubler(i):
+    return i*2
+
+doubler_using_lambda = lambda j:j*2
+
+
