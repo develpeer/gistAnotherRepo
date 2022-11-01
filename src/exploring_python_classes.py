8cdf63003f284083fe820df04ad0001144ea13bb
Dev E L'Peer <102568326+develpeer@users.noreply.github.com>
2022-10-31 17:29:37 -0700

    more class examples

--- src/exploring_python_classes.py
+++ src/exploring_python_classes.py
@@ -1,11 +1,12 @@
 
-class My_First_Class:
+class MY_FIRST_CLASS:
     """Should class names be camel case? 
     Who the fuck knows? And honestly how does it matter in a language 
     that actually uses white space for control flow. And  declarations 
     and semantics are implied by bullshit like __ and 3 quotes(which is idiotic) and stupid"""
 
     class_var = 100
+    class_var_2 = class_var+100
 
     __private_var__ = 200
 
@@ -14,24 +15,27 @@ class My_First_Class:
         self.o_var_2 = param
 
     def __repr__(self) -> str:
+        # this here is a great example of the bulshitteryu that is python control flow 
+        # If you want to remove leading spaces, you have to stick the lines to the left, which breaks 
+        # the visual control flow bullshit that python developers love so much 
         return f"""
-                classvar:{My_First_Class.class_var},
+                classvar:{MY_FIRST_CLASS.class_var},
+                classvar_2:{MY_FIRST_CLASS.class_var_2},
                 o_var_1:{self.o_var_1},
                 o_var_2:{self.o_var_2},                
                 """
                 
         pass
 
-o = My_First_Class(-1)
+o = MY_FIRST_CLASS(-1)
 print(o)
 
-print(dir(o))
-print(dir(My_First_Class))
+print("-"*40,"\n","All the attribites of o:\n","\n * ".join(dir(o)))
+print("-"*40,"\n","All the attribites of MyFirstClass:\n","\n * ".join(dir(MY_FIRST_CLASS)))
 
 del o.o_var_1
-
-print(o.o_var_1)
-
-
-
-    
+try:
+    print(o.o_var_1)
+except AttributeError as ae:
+    print(ae,("\n!!!!You can delete attributes of an object from outside the object.. WTF!!\n"
+            "So much for encapsulation"))    
