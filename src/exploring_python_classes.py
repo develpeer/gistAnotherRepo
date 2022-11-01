5c74a307bdd43830a4853d48c6025c486e274552
Dev E L'Peer <102568326+develpeer@users.noreply.github.com>
2022-10-31 18:18:45 -0700

    differentiating between object and class methods

--- src/exploring_python_classes.py
+++ src/exploring_python_classes.py
@@ -21,13 +21,26 @@ class MY_FIRST_CLASS:
         return f"""
                 classvar:{MY_FIRST_CLASS.class_var},
                 classvar_2:{MY_FIRST_CLASS.class_var_2},
-                o_var_1:{self.o_var_1},
+                o_var_1:{getattr(self,"o_var_1","UNDEFINED")},
                 o_var_2:{self.o_var_2},                
                 """
                 
         pass
 
-o = MY_FIRST_CLASS(-1)
+    def f1(*args):
+        for arg in args:
+            print("I got me some args y'all, because python " 
+            "uses an implicit invocation context to differentiate betwwen instance and class methods"
+            "\n That is if this method is an object method, you must declare its firt arg as 'self', which fuck that ..i'm going to"
+            "just call python_sucks..see next method"
+            "\n Stupid stupid python", f"arg = {arg}")
+        return "you get what you so.. do not deserve "
+
+    def f2(python_sucks):
+        return python_sucks.o_var_2 *100
+        
+
+o = MY_FIRST_CLASS(3.14)
 print(o)
 
 print("-"*40,"\n","All the attribites of o:\n","\n * ".join(dir(o)))
@@ -39,3 +52,11 @@ try:
 except AttributeError as ae:
     print(ae,("\n!!!!You can delete attributes of an object from outside the object.. WTF!!\n"
             "So much for encapsulation"))    
+
+print(f"MY_FIRST_CLASS.f1 {MY_FIRST_CLASS.f1}")
+print(f"MY_FIRST_CLASS.f1() {MY_FIRST_CLASS.f1()}")
+print(f"o.f1 {o.f1}")
+print(f"o.f1 {o.f1()}")
+print(f"o.f2 {o.f2}")
+print(f"o.f2 returned {o.f2()}")
+
